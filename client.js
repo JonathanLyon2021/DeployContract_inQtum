45b77c7de358c69d4165f26837d286ae9f17bcea
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-11-01 23:24:01 -0400

    Added contract address and ABI

--- client.js
+++ client.js
@@ -3,3 +3,8 @@ const username = 'username';
 const password = 'password';
 const rpcAddress = `http://${username}:${password}@localhost:13889`; //@localhost:13889
 const qweb = new Qweb3(rpcAddress);
+
+const fs = require("fs");
+const contractAddress = "1f2bcb283102523a7ced458da6026882d807b88c";
+let ABI = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi").toString();
+ABI = JSON.parse(ABI);
